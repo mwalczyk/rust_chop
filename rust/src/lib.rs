00b60418692d593e890881ab509217b47d89005e
Michael Walczyk <michael.walczyk@obscuradigital.com>
2018-01-22 14:01:21 -0800

    Adds safe wrapper around execute function.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,12 +3,7 @@ extern crate libc;
 use libc::{uint32_t, size_t, c_float};
 use std::slice;
 
-// Parameters:
-// chan_data: the float* of channel data from C++
-// chan_index: the numeric index of the channel
-// chan_samples: the number of samples in this channel
-// execute_count: the number of times this CHOP has executed
-
+// This function should not be modified! 
 #[no_mangle]
 pub extern fn execute_rs(chan_data: *mut c_float,		
 					     chan_index: size_t, 			
@@ -21,11 +16,32 @@ pub extern fn execute_rs(chan_data: *mut c_float,
 		slice::from_raw_parts_mut(chan_data, chan_samples as usize)
 	};
 
-	// Construct a sin wave
+	execute(&mut data, chan_index as usize, chan_samples as usize, execute_count as u32);
+}
+
+
+// This is a client-facing, safe version of the function above.
+// Authors should write their code inside of this function rather
+// than the unsafe block above.
+//
+// Parameters:
+// chan_data: the float* of channel data from C++
+// chan_index: the numeric index of the channel
+// chan_samples: the number of samples in this channel
+// execute_count: the number of times this CHOP has executed
+// 
+// This function will be called once for each output channel
+// of the CPlusPlus CHOP.
+pub fn execute(chan_data: &mut [f32],
+			   chan_index: usize,
+			   chan_samples: usize,
+			   execute_count: u32) {
+
+	// As a simple example, output a sin wave.
 	let phase = 2.0 * std::f64::consts::PI;
 	let sample_rate = 60.0;
 
-	for (index, elem) in data.iter_mut().enumerate() {
+	for (index, elem) in chan_data.iter_mut().enumerate() {
 		let percent = (index as f64) / (chan_samples as f64);
 		let timestep = (execute_count as f64) / sample_rate;
 		let val = (phase * percent + timestep).sin();
@@ -33,8 +49,5 @@ pub extern fn execute_rs(chan_data: *mut c_float,
     	*elem = val as f32;
 	}
 }
-
-
-
 
 
